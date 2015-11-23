d0009078060c2d87ae958fd15eb532606458a50d
Craig Ramey <craigramey@Craigs-MacBook-Air.local>
2015-11-23 11:21:39 -0500

    Handlebar template

---
+++ public/app.js
@@ -0,0 +1,17 @@
+var firstRender = function() {
+	$('#form-container').empty();
+	var source = $("#first-template").html();
+	var template = Handlebars.compile(source);
+
+	var context = {title: "My New Post", body: "This is my first post!"};
+	$('#form-container').append(template(context));
+};
+
+var secondRender = function() {
+	$('#form-container-two').empty();
+	var source = $('#second-template').html();
+	var template = Handlebars.compile(source);
+
+	var context = {title: "Testing my second post", body: "This is my second test, deal with it!"};
+	$('#form-container-two').append(template(context));
+};
