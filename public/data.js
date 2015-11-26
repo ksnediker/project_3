aa6463ee799ca7fa8b19319f5ba20c1ac2c123db
Luis Castaneda <luis.m.castaneda@gmail.com>
2015-11-26 16:23:34 -0500

    visual stuffs

--- public/data.js
+++ public/data.js
@@ -1,68 +1,63 @@
-$('document').ready( function () {
-
-
-
-
 // ajax call to get the data in survey2, copied below
 
-// var jsonData;
 
+// var jsonData;
 // $.ajax('http://localhost:3000/surveys').done( function(data){
 // 	jsonData = data;
+
 // });
 
+
+// given the following JSON object ---- accessed here as a variable or 
+// through an AJAX call, also above:
 
 var jsonData = {
     title: 'GA Survey',
      form1 : {
       question: "Which gender do you most closely identify with?",
-      answers : [{"Male": 35}, {"Female": 38},{"LGBTQ": 10}, {"Rather not disclose" : 10}]
+      answers : [{"Male": 35}, {"Female": 38},{"LGBTQ": 10}, {"Undisclosed" : 10}]
       },
     form2 : {
       question: "What is your age?",
-      answers : [ {"18-24": 30}, {"25-29": 25}, {"30-34": 15}, {"35-39": 13},{"40-44": 10}, {"45+": 10}]
+      answers : [ {"Eighteen_to_Twenty_Four": 30}, {"Twenty_Five_to_Twenty_Nine": 25}, {"Thirty_to_Thirty_Four": 15}, {"Thirty_Five_to_Thirty_Nine": 13},{"Forty_to_Forty_Four": 10}, {"Forty_Five_plus": 10}]
     },
 
     form3 : {
       question: "How many GA courses have you taken including this one?",
-      answers: [{"1": 31}, {"2-3": 35},{"4-5": 33},{"More than 5": 44} ]
+      answers: [{"One": 31}, {"Two_or_Three": 35},{"Four_or_Five": 33},{"Five_plus": 44} ]
     },
     form4 : {
       question: "What industry were you in prior to WDI?",
-      answers: [{"Healthcare": 15}, {"Finance": 15},{"Consulting": 17 }, {"Consulting": 20}, {"Technology" : 30}, {"Real Estate": 7}, {"Education" : 10}, {"Hospitality" : 4}, {"Student" : 10}, {"Other" : 18}]
+      answers: [{"Healthcare": 15}, {"Finance": 15}, {"Consulting": 20}, {"Technology" : 30}, {"Real_Estate": 7}, {"Education" : 10}, {"Hospitality" : 4}, {"Student" : 10}, {"Other" : 18}]
     },
 
     form5 : {
       question: "What would be your ideal position after WDI?",
-      answers: [{"Full Stack Developer": 5}, {"Front-end Developer": 15},{"Back-end Developer": 10}, {"Software Engineer": 8}, {"Database Administrator": 9}, {"Other": 5} ]
+      answers: [{"Full_Stack_Developer": 5}, {"Front_end_Developer": 15},{"Back_end_Developer": 10}, {"Software_Engineer": 8}, {"Database_Administrator": 9}, {"Other": 5} ]
     },
 
      form6 : {
-      question: "What would be your ideal position after WDI?",
-      answers: [{"The course is as difficult as I expected it to be": 15}, {"The course is more difficult that I expected it to be": 25},{"The course is less difficult that I expected it to be": 9}]
+      question: "How did WDI match your expectations?",
+      answers: [{"Class_is_as_difficult_as_I_expected": 15}, {"Class_is_more_difficult_than_I_expected": 25},{"Class_is_easier_than_I_expected": 9}]
     },
 
       form6 : {
-      question: "Which of the following most closely represents your feelings on the difficulties of the course compared to your expectations?",
-      answers: [{"0-24th percentile": 5}, {"25-49th percentile": 15},{"50-74th percentile": 27}, {"75-100th percentile":28}]
+      question: "Compared to the rest of the class, which percentile would you place yourself into in terms of your programming skills and abilities?",
+      answers: [{"Bottom_Quarter": 5}, {"Second_Quarter": 15},{"Third_Quarter": 27}, {"Top_Quarter":28}]
     },
     form7: {
-      question: " Compared to the rest of the class, which percentile would you place yourself into in terms of your programming skills and abilities?",
-      answers: [{"0-24th percentile" : 17}, {"25-49th percentile" :26}, {"50-74th percentile":28},{"75-100th percentile":25}]
+      question: "What is your favorite stack?", 
+      answers: [{"Ruby_on_Rails" : 30}, { "MEAN" : 20 }, {"Python_Django" : 10}, {"Other": 5}]
     },
     form8: {
-      question: "What is your favorite stack?", 
-      answers: [{"Ruby on Rails" : 30}, { "MEAN" : 20 }, {"Python - Django" : 10}]
+      question:" Is it pronounced 'Gif' or 'Jif'?",
+      answers: [{"Gif" : 18 }, { "Jif" : 28}, {"Who_cares": 5}]
     },
     form9: {
-      question:" Is it pronounced 'Gif' or 'Jif'?",
-      answers: [{"Gif" : 18 }, { "Jif" : 28}, {"Nobody cares": 5}]
+      question: "Marvel or DC?",
+      answers: [{ "Marvel": 4 }, {"DC" : 5}, {"Who_cares" :46}]
     },
     form10: {
-      question: "Marvel or DC?",
-      answers: [{ "Marvel": 4 }, {"DC" : 5}, {"Nobody cares" :46}]
-    },
-    form11: {
       question: "Dogs or cats?",
       answers: [{ "Dogs" : 5 }, {"Cats" :5}, {"Piglets" : 28}]
 
@@ -72,47 +67,210 @@ var jsonData = {
 
 console.log(jsonData);
 
+// ==================================================
+//  Using Chart.js to make some nice pie charts :) 
+// ==================================================
 
+// this is your data for the chart rendering gender breakdown
 
-var firstValue = jsonData.form1.answers[0].Male;
-var secondValue = jsonData.form1.answers[1].Female;
-var thirdValue = jsonData.form1.answers[2].LGBTQ;
+var genderFirstValue = jsonData.form1.answers[0].Male;
+var genderSecondValue = jsonData.form1.answers[1].Female;
+var genderThirdValue = jsonData.form1.answers[2].LGBTQ;
+var genderFourthValue = jsonData.form1.answers[3].Undisclosed;
 
-
-var pieData = [
+var genderPieData = [
                 {
 
-                    value: firstValue,
-                    color:"#878BB6"
+                    value: genderFirstValue,
+                    color:"#878BB6",
+                    label: "Male"
                 },
                 {
-                    value : secondValue,
-                    color : "#4ACAB4"
+                    value : genderSecondValue,
+                    color : "#4ACAB4",
+                    label: "Female"
                 },
                 {
-                    value: thirdValue,
-                    color : "#FF8153"
+                    value: genderThirdValue,
+                    color : "#FF8153",
+                    label: "LGBTQ"
+                },
+                {
+                    value: genderFourthValue,
+                    color : "#FFEA88",
+                    label: "Undisclosed"
                 }
-                
-                //     value: jsonData[0].form1.answers[0].Rather not disclose,
-                //     color : "#FFEA88"
-                // }
             ];
-
 
     var pieOptions = {
                  segmentShowStroke : false,
                  animateScale : true
             }
 
-var gender = document.getElementById("first_Question").getContext("2d");
-
-var myNewChart = new Chart(gender).Pie(pieData, pieOptions);
-
-
-
-});
+var gender = document.getElementById("gender").getContext("2d");
+// generates gender chart
+var newGenderChart = new Chart(gender).Pie(genderPieData, pieOptions);
 
 
+/// data for age pie chart
+var ageFirstValue = jsonData.form2.answers[0].Eighteen_to_Twenty_Four;
+var ageSecondValue = jsonData.form2.answers[1].Twenty_Five_to_Twenty_Nine;
+var ageThirdValue = jsonData.form2.answers[2].Thirty_to_Thirty_Four;
+var ageFourthValue = jsonData.form2.answers[3].Thirty_Five_to_Thirty_Nine;
+var ageFifthValue = jsonData.form2.answers[4].Forty_to_Forty_Four;
+var ageSixthValue = jsonData.form2.answers[5].Forty_Five_plus;
+ 
+var agePieData = [
+                {
 
+                    value: ageFirstValue,
+                    color:"#878BB6",
+                    label: "18-24" // we need strings in the keys to access values
+                    							// but we can render them as numerical labels :)	
+                },
+                {
+                    value : ageSecondValue,
+                    color : "#4ACAB4",
+                    label: "25-29"
+                },
+                {
+                    value: ageThirdValue,
+                    color : "#FF8153",
+                    label: "30-34"
+                },
+                {
+                    value: ageFourthValue,
+                    color : "#FFEA88",
+                    label: "35-39"
+                },
+                {
+                    value: ageFifthValue,
+                    color : "#FF00FF",
+                    label: "40-45"
+                },
+                {
+                    value: ageSixthValue,
+                    color : "#7FFFD4",
+                    label: "45+"
+                }
+            ];
+
+    var agePieOptions = {
+                 segmentShowStroke : false,
+                 animateScale : true
+            }
+
+var age = document.getElementById("age").getContext("2d");
+// generates age chart
+var newAgeChart = new Chart(age).Pie(agePieData, agePieOptions);
+
+/// data for GA Courses chart
+var coursesFirstValue = jsonData.form3.answers[0].One;
+var coursesSecondValue = jsonData.form3.answers[1].Two_or_Three;
+var coursesThirdValue = jsonData.form3.answers[2].Four_or_Five;
+var coursesFourthValue = jsonData.form3.answers[3].Five_plus;
+
+var coursesPieData = [
+                {
+
+                    value: coursesFirstValue,
+                    color:"#878BB6",
+                    label: "1" // we need strings in the keys to access values
+                    							// but we can render them as numerical labels :)	
+                },
+                {
+                    value : coursesSecondValue,
+                    color : "#4ACAB4",
+                    label: "2-3"
+                },
+                {
+                    value: coursesThirdValue,
+                    color : "#FF8153",
+                    label: "4-5"
+                },
+                {
+                    value: coursesFourthValue,
+                    color : "#FFEA88",
+                    label: "5+"
+                }
+            ];
+
+    var coursesPieOptions = {
+                 segmentShowStroke : false,
+                 animateScale : true
+            }
+
+var courses = document.getElementById("courses").getContext("2d");
+// generates age chart
+var newAgeChart = new Chart(courses).Pie(coursesPieData, coursesPieOptions);
+
+// data for industry pie chart
+var experienceFirstValue = jsonData.form4.answers[0].Healthcare;
+var experienceSecondValue = jsonData.form4.answers[1].Finance;
+var experienceThirdValue = jsonData.form4.answers[2].Consulting;
+var experienceFourthValue = jsonData.form4.answers[3].Technology;
+var experienceFifthValue = jsonData.form4.answers[4].Real_Estate;
+var experienceSixthValue = jsonData.form4.answers[5].Education;
+var experienceSeventhValue = jsonData.form4.answers[6].Hospitality;
+var experienceEighthValue = jsonData.form4.answers[7].Student;
+var experienceNinthValue = jsonData.form4.answers[8].Other;
+
+var experiencePieData = [
+                {
+
+                    value: experienceFirstValue,
+                    color:"#878BB6",
+                    label: "Healthcare" // we need strings in the keys to access values
+                    							// but we can render them as numerical labels :)	
+                },
+                {
+                    value : experienceSecondValue,
+                    color : "#4ACAB4",
+                    label: "Finance"
+                },
+                {
+                    value: experienceThirdValue,
+                    color : "#FF8153",
+                    label: "Consulting"
+                },
+                {
+                    value: experienceFourthValue,
+                    color : "#FFEA88",
+                    label: "Technology"
+                },
+                 {
+                    value: experienceFifthValue,
+                    color : "#FF00FF",
+                    label: "Real Estate"
+                },
+                 {
+                    value: experienceSixthValue,
+                    color : "#FFEA88",
+                    label: "Education"
+                },
+                 {
+                    value: experienceSeventhValue,
+                    color : "#87CEFA",
+                    label: "Hospitality"
+                },
+                 {
+                    value: experienceEighthValue,
+                    color : "#ADD8E6",
+                    label: "Student"
+                },
+                   {
+                    value: experienceNinthValue,
+                    color : "#FF69B4",
+                    label: "Other"
+                } 
+            ];
+
+    var experiencePieOptions = {
+                 segmentShowStroke : false,
+                 animateScale : true
+            }
+
+var experience = document.getElementById("experience").getContext("2d");
+// generates age chart
+var newAgeChart = new Chart(experience).Pie(experiencePieData, experiencePieOptions);
 
--- server.js
+++ server.js
@@ -99,60 +99,56 @@ app.get('/surveys', function(req, res){
 
   });
 });
+// sample survey
 
+// var survey1 = new Survey({
 
-
-// var survey2 = new Survey({
 //     title: 'GA Survey',
 //      form1 : {
 //       question: "Which gender do you most closely identify with?",
-//       answers : [{"Male": 35}, {"Female": 38},{"LGBTQ": 10}, {"Rather not disclose" : 10}]
+//       answers : [{"Male": 35}, {"Female": 38},{"LGBTQ": 10}, {"Undisclosed" : 10}]
 //       },
 //     form2 : {
 //       question: "What is your age?",
-//       answers : [ {"18-24": 30}, {"25-29": 25}, {"30-34": 15}, {"35-39": 13},{"40-44": 10}, {"45+": 10}]
+//       answers : [ {"Eighteen_to_Twenty_Four": 30}, {"Twenty_Five_to_Twenty_Nine": 25}, {"Thirty_to_Thirty_Four": 15}, {"Thirty_Five_to_Thirty_Nine": 13},{"Forty_to_Forty_Four": 10}, {"Forty_Five_plus": 10}]
 //     },
 
 //     form3 : {
 //       question: "How many GA courses have you taken including this one?",
-//       answers: [{"1": 31}, {"2-3": 35},{"4-5": 33},{"More than 5": 44} ]
+//       answers: [{"One": 31}, {"Two_or_Three": 35},{"Four_or_Five": 33},{"Five_plus": 44} ]
 //     },
 //     form4 : {
 //       question: "What industry were you in prior to WDI?",
-//       answers: [{"Healthcare": 15}, {"Finance": 15},{"Consulting": 17 }, {"Consulting": 20}, {"Technology" : 30}, {"Real Estate": 7}, {"Education" : 10}, {"Hospitality" : 4}, {"Student" : 10}, {"Other" : 18}]
+//       answers: [{"Healthcare": 15}, {"Finance": 15}, {"Consulting": 20}, {"Technology" : 30}, {"Real_Estate": 7}, {"Education" : 10}, {"Hospitality" : 4}, {"Student" : 10}, {"Other" : 18}]
 //     },
 
 //     form5 : {
 //       question: "What would be your ideal position after WDI?",
-//       answers: [{"Full Stack Developer": 5}, {"Front-end Developer": 15},{"Back-end Developer": 10}, {"Software Engineer": 8}, {"Database Administrator": 9}, {"Other": 5} ]
+//       answers: [{"Full_Stack_Developer": 5}, {"Front_end_Developer": 15},{"Back_end_Developer": 10}, {"Software_Engineer": 8}, {"Database_Administrator": 9}, {"Other": 5} ]
 //     },
 
 //      form6 : {
-//       question: "What would be your ideal position after WDI?",
-//       answers: [{"The course is as difficult as I expected it to be": 15}, {"The course is more difficult that I expected it to be": 25},{"The course is less difficult that I expected it to be": 9}]
+//       question: "How did WDI match your expectations?",
+//       answers: [{"Class_is_as_difficult_as_I_expected": 15}, {"Class_is_more_difficult_than_I_expected": 25},{"Class_is_easier_than_I_expected": 9}]
 //     },
 
 //       form6 : {
-//       question: "Which of the following most closely represents your feelings on the difficulties of the course compared to your expectations?",
-//       answers: [{"0-24th percentile": 5}, {"25-49th percentile": 15},{"50-74th percentile": 27}, {"75-100th percentile":28}]
+//       question: "Compared to the rest of the class, which percentile would you place yourself into in terms of your programming skills and abilities?",
+//       answers: [{"Bottom_Quarter": 5}, {"Second_Quarter": 15},{"Third_Quarter": 27}, {"Top_Quarter":28}]
 //     },
 //     form7: {
-//       question: " Compared to the rest of the class, which percentile would you place yourself into in terms of your programming skills and abilities?",
-//       answers: [{"0-24th percentile" : 17}, {"25-49th percentile" :26}, {"50-74th percentile":28},{"75-100th percentile":25}]
+//       question: "What is your favorite stack?", 
+//       answers: [{"Ruby_on_Rails" : 30}, { "MEAN" : 20 }, {"Python_Django" : 10}, {"Other": 5}]
 //     },
 //     form8: {
-//       question: "What is your favorite stack?", 
-//       answers: [{"Ruby on Rails" : 30}, { "MEAN" : 20 }, {"Python - Django" : 10}]
+//       question:" Is it pronounced 'Gif' or 'Jif'?",
+//       answers: [{"Gif" : 18 }, { "Jif" : 28}, {"Who_cares": 5}]
 //     },
 //     form9: {
-//       question:" Is it pronounced 'Gif' or 'Jif'?",
-//       answers: [{"Gif" : 18 }, { "Jif" : 28}, {"Nobody cares": 5}]
+//       question: "Marvel or DC?",
+//       answers: [{ "Marvel": 4 }, {"DC" : 5}, {"Who_cares" :46}]
 //     },
 //     form10: {
-//       question: "Marvel or DC?",
-//       answers: [{ "Marvel": 4 }, {"DC" : 5}, {"Nobody cares" :46}]
-//     },
-//     form11: {
 //       question: "Dogs or cats?",
 //       answers: [{ "Dogs" : 5 }, {"Cats" :5}, {"Piglets" : 28}]
 
@@ -161,78 +157,9 @@ app.get('/surveys', function(req, res){
 //   });
 
 
-
-// var survey1 = new Survey({
-//     title: 'GA Survey',
-//      form1 : {
-//       question: "Which gender do you most closely identify with?",
-//       answers : [{"Male": 0}, {"Female": 0},{"LGBTQ": 0}, {"Rather not disclose" : 0}]
-//       },
-//     form2 : {
-//       question: "What is your age?",
-//       answers : [ {"18-24": 0}, {"25-29": 0}, {"30-34": 0}, {"35-39": 0},{"40-44": 0}, {"45+": 0}]
-//     },
-
-//     form3 : {
-//       question: "How many GA courses have you taken including this one?",
-//       answers: [{"1": 0}, {"2-3": 0},{"4-5": 0},{"More than 5": 0} ]
-//     },
-//     form4 : {
-//       question: "What industry were you in prior to WDI?",
-//       answers: [{"Healthcare": 0}, {"Finance": 0},{"Consulting": 0}, {"Consulting": 0}, {"Technology" : 0}, {"Real Estate": 0}, {"Education" : 0}, {"Hospitality" : 0}, {"Student" : 0}, {"Other" : 0}]
-//     },
-
-//     form5 : {
-//       question: "What would be your ideal position after WDI?",
-//       answers: [{"Full Stack Developer": 0}, {"Front-end Developer": 0},{"Back-end Developer": 0}, {"Software Engineer": 0}, {"Database Administrator": 0}, {"Other": 0} ]
-//     },
-
-//      form6 : {
-//       question: "What would be your ideal position after WDI?",
-//       answers: [{"The course is as difficult as I expected it to be": 0}, {"The course is more difficult that I expected it to be": 0},{"The course is less difficult that I expected it to be": 0}]
-//     },
-
-//       form6 : {
-//       question: "Which of the following most closely represents your feelings on the difficulties of the course compared to your expectations?",
-//       answers: [{"0-24th percentile": 0}, {"25-49th percentile": 0},{"50-74th percentile": 0}, {"75-100th percentile":0}]
-//     },
-//     form7: {
-//       question: " Compared to the rest of the class, which percentile would you place yourself into in terms of your programming skills and abilities?",
-//       answers: [{"0-24th percentile" : 0}, {"25-49th percentile" :0}, {"50-74th percentile":0},{"75-100th percentile":0}]
-//     },
-//     form8: {
-//       question: "What is your favorite stack?", 
-//       answers: [{"Ruby on Rails" : 0}, { "MEAN" : 0 }, {"Python - Django" : 0}]
-//     },
-//     form9: {
-//       question:" Is it pronounced 'Gif' or 'Jif'?",
-//       answers: [{"Gif" : 0 }, { "Jif" : 0}, {"Nobody cares": 0}]
-//     },
-//     form10: {
-//       question: "Marvel or DC?",
-//       answers: [{ "Marvel": 0 }, {"DC" : 0}, {"Nobody cares" :0}]
-//     },
-//     form11: {
-//       question: "Dogs or cats?",
-//       answers: [{ "Dogs" :0 }, {"Cats" :0}, {"Piglets" : 0}]
-
-//     }
-
-//   });
-
-
-// sample survey
-// var survey1 = new Survey({
-//     title: 'Our Dog Survey',
-//     form1 : {
-//       question: "What is your favorite dog?",
-//       answers: [{Labrador: 1}, {Beagle: 1},{Poodle: 1}]
-//     }
-// });
-
-// survey2.save(function (err) {
+// survey1.save(function (err) {
 //   if (err) console.log(err);
-//   console.log(survey2.title + ' created');
+//   console.log(survey1.title + ' created');
 // })
 
 // 
