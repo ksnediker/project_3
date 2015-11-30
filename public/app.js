d900b88b0022e6f8e8a38397b3cd025fd55737e7
Craig Ramey <craigramey@Craigs-MacBook-Air.local>
2015-11-30 15:09:51 -0500

    heroku fun

--- public/app.js
+++ public/app.js
@@ -11,6 +11,8 @@ $(document).ready(function(){
 	var wordCloud = null;
 	var inputWords = null;
 	var splitArr = null;
+	var radarValues = [];
+  var radarLabels = [];
 
 	var renderFirstTemplate = function() {
 		changeData = [];
@@ -26,7 +28,8 @@ $(document).ready(function(){
 					$('#word-cloud-question').show();
 					$('#submit-cloud').click(function(){
 						inputWords = $('#submit-words').val();
-						splitArr = inputWords.split(" ");
+						var lowerWords = inputWords.toLowerCase();
+						splitArr = lowerWords.split(" ");
 						pushToCloud(splitArr)
 						changeData.push(wordCloud)
 						if (!(form in changeData[0])) {
@@ -35,7 +38,6 @@ $(document).ready(function(){
 						}
 						$('#word-cloud-question').toggle("fold");
 						$('#chart-container').remove();
-						renderWordCloud();
 						$('.word-cloud').toggle("fold");
 						updateData();
 					})
@@ -45,13 +47,12 @@ $(document).ready(function(){
 	  		
 			   		$.each((data[0][form].answers[0]), function(index, value){
 			   				arr.push(index);
-			   				// tempArr.replace("_", " ").push(index);
 			   		});
 
 			   		for (var m = 0; m < arr.length; m ++) {
 			   			renderArr.push(arr[m].replace("_", " "));
 			   		}
-			   		console.log(arr)
+
 			   		var context = {
 			   				title: data[0][form, question], 
 			   				body: arr
@@ -66,11 +67,12 @@ $(document).ready(function(){
 								if (form === "form3") {
 									keyValues.push(data[0][form].answers[0][arr[k]]);
 									barGraphLabels = renderArr;
+								} else if ( form === "form4") {
+									radarValues.push(data[0][form].answers[0][arr[k]]);
+      						radarLabels = renderArr;
 								}
 								if ($( '#' + k ).is(':checked')){
-									console.log(arr[k])
-									
-									// var $getKeyName = $( '#' + k ).val()
+
 									var $getKeyName = arr[k];
 									data[0][form].answers[0][$getKeyName] = data[0][form].answers[0][$getKeyName] + 1
 									changeData.push(data[0][form].answers[0])
@@ -80,48 +82,11 @@ $(document).ready(function(){
 									var newKey = form;
 									changeData[0][newKey] = 1;
 								} 							
-						console.log(changeData)
 						updateData();
 					});
 		   		}
 	    });
 	}
-
-	// var renderSecondTemplate = function() {
-	// 	var source = $("#second-template").html();
-	// 	var template = Handlebars.compile(source);
-		
-	// 	$.ajax('http://localhost:3000/surveys').done( function(data){
-
-			
-	//   		console.log(form)
-	//   		console.log((data[0][form].question))
-	// 			var context = {
-	//    				title: data[0][form].question, 
-	//    				body: arr
-	//    			};
-	  			
-	//    		$.each((data[0][form].answers[0]), function(index, value){
-	//    				arr.push(index);
-	//    		});
-	// 			$('#modal').show();
-	//    		id = data[0]._id;
-
-	//    		$('#form-container').append(template(context));
-	//    		$('#submit-answer').click(function(){
-
-	// 				for (var k = 0; k < arr.length; k++) {
-	// 					if ($( '#' + k ).is(':checked')){
-	// 						var $getKeyName = $( '#' + k ).val()
-	// 						data[0][form].answers[0][$getKeyName] = data[0][form].answers[0][$getKeyName] + 1
-	// 						changeData.push(data[0][form].answers[0])													
-	// 					} 
-	// 				}
-	// 				$('#form-container').empty();
-	// 				updateData();
-	// 			});
-	//     });
-	// }
 
 	var currentForm = function( number ) {
     return form = "form" + number;
@@ -170,31 +135,6 @@ $(document).ready(function(){
 		renderFirstTemplate();
 	}
 
-	// var seventhQuestion = function() {
-	// 	console.log("Hello");
-	// 	currentForm(7);
-	// 	currentQuestion(7);
-	// 	renderFirstTemplate();
-	// }
-
-	// var eigthQuestion = function() {
-	// 	currentForm(8);
-	// 	currentQuestion(8);
-	// 	renderFirstTemplate();
-	// }
-
-	// var ninthQuestion = function() {
-	// 	currentForm(9);
-	// 	currentQuestion(9);
-	// 	renderFirstTemplate();
-	// }
-
-	// var tenthQuestion = function() {
-	// 	currentForm(10);
-	// 	currentQuestion(10);
-	// 	renderFirstTemplate();
-	// }
-
 	var chartRender = function() {
 		$('#chart-container').show();
 		$('#modal').toggle('fold');
@@ -203,49 +143,51 @@ $(document).ready(function(){
 
 			if (form === "form3") {
 				makeBarChart();
-			} else {
+			} else if (form === "form4") {
+      		makeRadarChart();
+        }
+      else {
+
+				var answerNames = Object.keys(data[0][form].answers[0]);
+				var pieData = [];
 
 
-			var answerNames = Object.keys(data[0][form].answers[0]);
-			var pieData = [];
+				for (var d = 0; d < answerNames.length; d++) {
+					
+					var pieValue = data[0][form].answers[0][answerNames[d]]
 
+					pieData.push({
+						value: pieValue, 
+						color: getRandomColor(), 
+						label: answerNames[d]
+					})
 
-			for (var d = 0; d < answerNames.length; d++) {
-				
-				var pieValue = data[0][form].answers[0][answerNames[d]]
+					var legendColor = pieData[d].color;
 
-				pieData.push({
-					value: pieValue, 
-					color: getRandomColor(), 
-					label: answerNames[d]
-				})
+					$('#legend-list').append("<li class='block' id='block" + d + "'></li>");
+					$('#block' + d).css({
+						"background-color": legendColor,
+						"height": "10%",
+						"width": "100%",
+						"text-shadow": "white 1px 1px"
+					}).append("<h3>     " + answerNames[d] + ":  " + pieValue + "</h3>");;
+				}
 
-				var legendColor = pieData[d].color;
+				var pieOptions = {
+				  segmentShowStroke : true,
+				 	segmentStrokeColor: "rgb(48,48,48)",
+				  animateScale : true,
+				}
 
-				$('#legend-list').append("<li class='block' id='block" + d + "'></li>");
-				$('#block' + d).css({
-					"background-color": legendColor,
-					"height": "10%",
-					"width": "100%",
-					"text-shadow": "white 1px 1px"
-				}).append("<h3>     " + answerNames[d] + ":  " + pieValue + "</h3>");;
+				$('#chart-container').prepend("<canvas id='show-chart'></canvas>");
+		   	var $showChart = $('#show-chart').get(0).getContext("2d");
+				var newChart = new Chart($showChart).Pie(pieData, pieOptions);
+				$('#show-chart').append(newChart).css({
+					"display": "inline",
+					"top": "50",
+					"right": "400"
+				});
 			}
-
-			var pieOptions = {
-			  segmentShowStroke : true,
-			 	segmentStrokeColor: "rgb(48,48,48)",
-			  animateScale : true,
-			}
-
-			$('#chart-container').prepend("<canvas id='show-chart'></canvas>");
-	   	var $showChart = $('#show-chart').get(0).getContext("2d");
-			var newChart = new Chart($showChart).Pie(pieData, pieOptions);
-			$('#show-chart').append(newChart).css({
-				"display": "inline",
-				"top": "50",
-				"right": "400"
-			});
-		}
 		});
 		arr = [];
 	};
@@ -257,7 +199,7 @@ $(document).ready(function(){
     	labels: barGraphLabels,
 	    datasets: [
 	      {
-	        label: "My First dataset",
+	        label: "Bar Chart",
 	        fillColor: "rgb(255, 179, 71)",
 	        strokeColor: "rgb(255, 179, 71)",
 	        highlightFill: "rgba(149, 43, 29, .4)",
@@ -279,6 +221,36 @@ $(document).ready(function(){
 				"right": "400"
 			});
 	}
+
+	var makeRadarChart = function() {
+
+    var data = {
+      labels: radarLabels,
+      datasets: [
+	        {
+	          label: "Radar Chart",
+	          fillColor: "rgb(255, 179, 71)",
+	          strokeColor: "rgb(255, 179, 71)",
+	          highlightFill: "rgba(149, 43, 29, .4)",
+	          highlightStroke: "rgba(149, 43, 29, .4)",
+	          data: radarValues
+	        }
+        ]
+      };
+       
+       var barOptions = {
+          scaleBeginAtZero : true
+        }
+
+        $('#chart-container').prepend("<canvas id='show-chart'></canvas>");
+          var $showChart = $('#show-chart').get(0).getContext("2d");
+          var newChart = new Chart($showChart).Radar(data, barOptions);
+          $('#show-chart').append(newChart).css({
+            "display": "inline",
+            "top": "50",
+            "right": "400"
+          });
+  }
 
 	$('#submit-email').click(function(){
 		// if (!document.cookie) {
@@ -338,54 +310,17 @@ $(document).ready(function(){
   	getWordCloud();
 	});
 
-	// $('#seventh-question').click(function(){
-	// 	$('#chart-container').toggle( "fold" );
-	// 	$('canvas').remove();
-	// 	$('#legend-list').empty();
- //  	$('#seventh-question').hide();
- //  	$('#eigth-question').show();
- //  	seventhQuestion();
-	// });
-
-	// $('#eigth-question').click(function(){
-	// 	$('#chart-container').toggle( "fold" );
-	// 	$('canvas').remove();
-	// 	$('#legend-list').empty();
- //  	$('#eigth-question').hide();
- //  	$('#ninth-question').show();
- //  	eigthQuestion();
-	// });
-
-	// $('#ninth-question').click(function(){
-	// 	$('#chart-container').toggle( "fold" );
-	// 	$('canvas').remove();
-	// 	$('#legend-list').empty();
- //  	$('#ninth-question').hide();
- //  	$('#tenth-question').show();
- //  	ninthQuestion();
-	// });
-
-	// $('#tenth-question').click(function(){
-	// 	$('#chart-container').toggle( "fold" );
-	// 	$('canvas').remove();
-	// 	$('#legend-list').empty();
- //  	$('#tenth-question').hide();
- //  	tenthQuestion();
-	// });
 
 	var updateData = function() {
-		console.log(changeData)
-		console.log(changeData[0])
+
 		$.ajax({
 			url: "http://localhost:3000/surveys/" + id,
 			method: "PUT",
 			data: changeData[0]
 		});
 			if (form === "form6") {
-				// renderWordCloud();
-				console.log("word cloud");
-				// $('#word-cloud-question').toggle("fold");
-				// $('.word-cloud').toggle("fold");
+				 delete wordCloud[form];
+				 renderWordCloud();
 			} else {
 					chartRender();
 			}							
@@ -410,19 +345,11 @@ $(document).ready(function(){
     console.log(user);
   };
 
-
-	// var wordCloud = {
-	// 	"large": 10, 
-	// 	"awesome": 1, 
-	// 	"Amazing": 20
-	// };
-
-
 	var pushToCloud = function(array) {
 
 		for (var i = 0; i < array.length; i++) {	
-			if (!(array[i] in wordCloud)) {
-				var newKey = array[i].toLowerCase();
+			var newKey = array[i].toLowerCase();
+			if (!(newKey in wordCloud)) {
 				wordCloud[newKey] = 1;
 			} else if (array[i] in wordCloud) {
 				wordCloud[array[i]] = wordCloud[array[i]] + 1
@@ -438,21 +365,17 @@ $(document).ready(function(){
 
 	var renderWordCloud = function() {
 		var sortedKeyValue = Object.keys(wordCloud).sort(function(a, b) {return -(wordCloud[a] - wordCloud[b])});
+		console.log(sortedKeyValue)
 		for (var x = 0; x < 6; x++) {
-			// $('.word-cloud').append("<h" + (x + 1) + ">");
 			$('.word-cloud').append("<h" + (x + 1) + ">" + sortedKeyValue[x] + "</h" + (x + 1) + ">");
 		};
 
-		// $('.word-cloud').find('h1').each(function(num){
-		// 	$(this).append( sortedKeyValue[num])
-		// })
 		for (var k = 6; k < sortedKeyValue.length; k++) {
-			// $('.word-cloud').append("<p id='word" + (k - 5) + "'>" + sortedKeyValue[k] + "</p>");
 			$('.word-cloud').append('<p>');
 		};
 		
 		$('.word-cloud').find('p').each(function (num) {
-		  $(this).append( sortedKeyValue[num]).css({
+		  $(this).append( sortedKeyValue[num + 6]).css({
 		    'position': 'absolute', 
 		    'top': randomNumber,
 		 		'left': randomNumber,
